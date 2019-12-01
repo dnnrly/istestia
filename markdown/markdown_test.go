e14a63ed598da1d08a7a2e87aeae8eeb111a0e2a
Pascal Dennerly <dnnrly@gmail.com>
2019-12-01 22:45:33 +0000

    Add additional validation for markdown

--- markdown/markdown_test.go
+++ markdown/markdown_test.go
@@ -26,3 +26,23 @@ func TestExtract_single(t *testing.T) {
 	assert.Equal(t, "go", data[0].Type)
 	assert.Equal(t, "package pkg\n", data[0].Contents)
 }
+
+func TestExtract_multiple(t *testing.T) {
+	data, err := Extract(mustLoad("test/multiple.md"))
+
+	assert.NoError(t, err)
+	require.Equal(t, 2, len(data))
+	assert.Equal(t, "go", data[0].Type)
+	assert.Equal(t, "package p1\n", data[0].Contents)
+	assert.Equal(t, "go", data[1].Type)
+	assert.Equal(t, "package p2\n", data[1].Contents)
+}
+
+func TestExtract_wrongLanguage(t *testing.T) {
+	data, err := Extract("\n```python\nvar a = 0\n")
+
+	assert.NoError(t, err)
+	require.Equal(t, 1, len(data))
+	assert.Equal(t, "python", data[0].Type)
+	assert.Equal(t, "var a = 0\n", data[0].Contents)
+}
